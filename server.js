244f504389cfac4d7a553a93c1a6b28907832b36
Merlin <jialing.huang@hotmail.com>
2021-02-07 13:06:32 -0500

    Updated files

--- server.js
+++ server.js
@@ -11,11 +11,11 @@ const http = require('http');
 const app = require('./backend/app');
 const port = process.env.PORT || 8080;  //Create a local server port as 3000
 
-app.use(express.static('dist/student-admin/'));
+/* app.use(express.static('dist/student-admin/'));
 
 app.get('/*',function(req,res){
     res.sendFile('index.html',{root:'dist/student-admin/'});
-}); 
+}); */ 
 
 app.set('port',port);
 const server = http.createServer(app);
